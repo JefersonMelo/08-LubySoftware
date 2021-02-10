ea4ed1b86157bdeecffec8c75c1a1b7aee4f98c5
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-10 18:09:31 -0300

    commit inicial

---
+++ 01-Logica_de_Programacao/CalcularPremio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcularPremio
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
