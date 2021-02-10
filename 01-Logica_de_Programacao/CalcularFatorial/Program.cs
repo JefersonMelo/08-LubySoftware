dc0e4aeb50e1d9db0258440f51cc7fc13a5f1d7b
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-10 18:07:05 -0300

    commit de conclusão.

--- 01-Logica_de_Programacao/CalcularFatorial/Program.cs
+++ 01-Logica_de_Programacao/CalcularFatorial/Program.cs
@@ -4,9 +4,33 @@ namespace CalcularFatorial
 {
     class Program
     {
+        /*
+        1.1 Implemente a função abaixo para calcular fatorial de um número.
+        // 5! = 5 · 4 · 3 · 2 · 1 = 120
+        CalcularFatorial(5) == 120//true 
+         */
+
+        static int fatorial( int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * fatorial(n - 1);
+            }
+        }
+
         static void Main( string[] args )
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enre com o Valor: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int resultado = fatorial(n);
+
+            Console.WriteLine(resultado);
+
         }
     }
 }
