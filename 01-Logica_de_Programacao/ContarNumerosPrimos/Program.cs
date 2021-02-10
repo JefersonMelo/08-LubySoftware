44eda156b401122fa512bccf0d0ff4b8bf6c5af6
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-10 20:30:15 -0300

    commit inicial

---
+++ 01-Logica_de_Programacao/ContarNumerosPrimos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContarNumerosPrimos
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
