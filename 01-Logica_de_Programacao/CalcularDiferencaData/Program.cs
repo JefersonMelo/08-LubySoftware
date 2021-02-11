dddebb14af50869324b8b4cf46aad223f14f3950
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-10 22:16:20 -0300

    commit inicial

---
+++ 01-Logica_de_Programacao/CalcularDiferencaData/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalcularDiferencaData
+{
+    class Program
+    {
+        /*
+        1.6 Implemente a função abaixo que obtém duas string de datas e calcula a diferença de dias entre elas.
+        CalcularDiferencaData("10122020", "25122020") == 15; //true  
+         */
+
+        static int CalcularDiferencaData( string a, string b)
+        {
+
+        }
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
