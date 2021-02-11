c87dd18a5dd082b8be17f87040317cbd2d371c95
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-11 10:53:50 -0300

    commmit inicial

---
+++ 01-Logica_de_Programacao/TransformarEmMatriz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TransformarEmMatriz
+{
+    class Program
+    {
+        /*
+        1.9 Implemente a função abaixo que obtém uma string com números separados por vírgula e transforma em um array de array de 
+        inteiros com no máximo dois elementos.
+        TransformarEmMatriz("1,2,3,4,5,6") == new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 5, 6 } }; //true  
+         */
+
+        static void Main( string[] args )
+        {
+            
+        }
+    }
+}
