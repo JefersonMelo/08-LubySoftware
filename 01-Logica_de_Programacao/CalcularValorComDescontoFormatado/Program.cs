e761a2688726c9e2c5e2068a1d643610ea9e39fd
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-10 21:18:56 -0300

    commit inicial

---
+++ 01-Logica_de_Programacao/CalcularValorComDescontoFormatado/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CalcularValorComDescontoFormatado
+{
+    class Program
+    {
+        /*
+        1.5 Implemente a função abaixo que aplica uma porcentagem de desconto a um valor e retorna o resultado.
+        Lembre-se que as entradas e saídas dos dados são strings que devem ser tratadas.
+        CalcularValorComDescontoFormatado("R$ 6.800,00", "30%") == "R$ 4.760,00"; //true  
+         */
+
+        static string CalcularValorComDescontoFormatado( )
+        {
+
+        }
+
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
