880f7565b423283ace8f8be23dd29de017375f6d
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-10 21:16:20 -0300

    commit final.

---
+++ 01-Logica_de_Programacao/CalcularVogais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CalcularVogais
+{
+    class Program
+    {
+        /*
+        1.4 Implemente a função abaixo que conta e calcula a quantidade de vogais dentro de uma string.
+        CalcularVogais("Luby Software") == 4//true 
+        https://docs.microsoft.com/pt-br/dotnet/api/system.char?view=net-5.0
+         */
+
+        static int CalcularVogais( string s )
+        {
+            int cont = 0;
+            foreach (var ch in s)
+            {
+                if (ch.ToString().ToLower() == "a" ||
+                    ch.ToString().ToLower() == "e" ||
+                    ch.ToString().ToLower() == "i" ||
+                    ch.ToString().ToLower() == "o" ||
+                    ch.ToString().ToLower() == "u")
+                {
+                    cont++;
+                }
+            }
+            return cont;
+        }
+
+        static void Main( string[] args )
+        {
+            string s = Console.ReadLine();
+
+            Console.WriteLine(CalcularVogais(s));
+        }
+    }
+}
