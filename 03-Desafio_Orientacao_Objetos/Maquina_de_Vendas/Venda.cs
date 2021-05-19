5a7e60a2d2c358c77f86fc4cae691b0061f183f4
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-19 12:31:44 -0300

    commit respostas REDES DE COMPUTADORES unidade i

--- 03-Desafio_Orientacao_Objetos/Maquina_de_Vendas/Venda.cs
+++ 03-Desafio_Orientacao_Objetos/Maquina_de_Vendas/Venda.cs
@@ -4,6 +4,17 @@ using System.Text;
 
 namespace Maquina_de_Vendas
 {
+     /*
+        1.9 Implemente a função abaixo que obtém uma string com números separados por vírgula e transforma em um array de array de 
+        inteiros com no máximo dois elementos.
+        TransformarEmMatriz("1,2,3,4,5,6") == new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 5, 6 } }; //true 
+        https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/arrays/jagged-arrays
+        https://www.portugal-a-programar.pt/forums/topic/10201-ajuda-cria%C3%A7%C3%A3o-de-matrizes-em-c/
+        https://pt.stackoverflow.com/questions/145897/diferen%C3%A7as-entre-jagged-array-e-multidimensional-array-em-c
+        https://dev.to/codebuns/c-jagged-array-array-of-arrays-f7g
+        https://www.alura.com.br/conteudo/csharp-collections
+        http://www.linhadecodigo.com.br/artigo/3444/arrays-em-csharp-teoria-e-pratica.aspx
+         */
     class Venda
     {        
         public double vendido( Produto p )
